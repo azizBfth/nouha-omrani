97e2933752863681911093e86cebfc3067ace7be
azizBfth <boufath.aziz83@gmail.com>
2023-12-14 16:07:35 +0100

    m

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className="pt-16 px-12 mt-8">
         <div>
           <h2 className="text-4xl text-slate-900 font-roboto-slab dark:text-slate-50 font-bold relative after:contents[] after:w-36 after:h-[2px] after:bg-primary after:absolute after:top-1/2 -translate-y-1/2 after:right-8 after:left-80">
-            Aprops de Moi
+          À propos de Moi
           </h2>
           <p className="text-gray-700 dark:text-slate-500 leading-7 font-medium">
             Une nouvelle diplômée en génie chimique industriel,ayant effectuée
